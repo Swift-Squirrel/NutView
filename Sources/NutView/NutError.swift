6b5a6e4c390b69476ffb022714ee74b4c1eaaf94
Filip Klembara <filip@klembara.pro>
2017-10-16 17:30:05 +0200

    Nut error as html

--- Sources/NutView/NutError.swift
+++ Sources/NutView/NutError.swift
@@ -95,6 +95,7 @@ public struct NutParserError: SquirrelError {
 
 /// Error struct for common errors in NutView
 public struct NutError: SquirrelError {
+
     /// Error kinds
     ///
     /// - notExists: File does not exists
@@ -122,5 +123,83 @@ public struct NutError: SquirrelError {
     init(kind: ErrorKind, description: String? = nil) {
         self.kind = kind
         self._description = description
+    }
+}
+
+// MARK: - SquirrelHTMLConvertibleError
+extension NutError: SquirrelHTMLConvertibleError {
+
+    /// HTML error representation
+    public var htmlErrorRepresentation: String {
+        switch kind {
+        case .notExists(let name):
+            return htmlTemplate(
+                title: "Nut file does not exists",
+                body: "File name: <i>\(name)</i>")
+        }
+    }
+}
+
+extension NutParserError: SquirrelHTMLConvertibleError {
+
+    /// HTML error representation
+    public var htmlErrorRepresentation: String {
+        func temp(title: String?, body: String) -> String {
+            let fileName = name ?? "Uknown file"
+            let res = """
+            <h4>File: <i>\(fileName)</i></h4>
+            <h4>Line: \(line)</h4>
+            <div>
+            \(body)
+            </div>
+            """
+            return htmlTemplate(title: title, body: res)
+        }
+        let html: String
+        switch kind {
+        case .evaluationError(let infix, let message):
+            let body = "Evaluation error in '\(infix)' (\(message))".escaped
+            html = temp(title: "Evaluation error", body: body)
+        case .expressionError:
+            html = temp(title: "Expression Error", body: "")
+        case .missingValue(let name):
+            html = temp(title: "Missing value", body: "Missing value for \(name.escaped)")
+        case .syntaxError(let expected, let got):
+            let body = """
+            Expecting one of:
+            <ul style="list-style: none">
+            \(expected.map({ "<li>\($0.escaped)</li>\n" }))
+            </ul>
+            But got: '\(got.escaped)'
+            """
+            html = temp(title: "Syntax Error", body: body)
+        case .unexpectedBlockEnd:
+            html = temp(title: "Unexpected block end", body: "Unexpected '\\}'".escaped)
+        case .unexpectedEnd(let reading):
+            html = temp(
+                title: "Unexpected end",
+                body: ("Unexpected end of file or using another command "
+                    + "while reading: '\(reading)'").escaped)
+        case .unknownInternalError(let commandName):
+            html = temp(
+                title: "Unknown internal error",
+                body: "Uknown error in '\(commandName)'".escaped)
+        case .wrongChainedVariable(let name, let command, let regex):
+            html = temp(
+                title: "Wrong variable name",
+                body: ("Variable '\(name)' in '\(command)' does not match "
+                    + "regular expression: \(regex)").escaped)
+        case .wrongSimpleVariable(let name, let command, let regex):
+            html = temp(
+                title: "Wrong variable name",
+                body: ("Variable '\(name)' in '\(command)' does not match "
+                    + "regular expression: \(regex)").escaped)
+        case .wrongValue(let name, let expected, let got):
+            html = temp(
+                title: "Wrong value",
+                body: ("Wrong value for '\(name)', expected type was '\(expected)' "
+                    + "but got '\(got)'").escaped)
+        }
+        return html
     }
 }
