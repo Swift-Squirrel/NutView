fd32dae0bd5b702823065f65e4456de9a87accb8
Filip Klembara <filip@klembara.pro>
2017-10-16 15:04:26 +0200

    add missing tests to LinuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,9 +1,13 @@
 import XCTest
 @testable import NutViewTests
+@testable import NutViewIntegrationTests
 
 XCTMain([
 	testCase(TokenTests.allTests),
     testCase(NutParserTests.allTests),
     testCase(NutParserErrors.allTests),
-    testCase(FruitParserTests.allTests)
+    testCase(FruitParserTests.allTests),
+    testCase(NutResolverTests.allTests),
+    testCase(InterpreterTests.allTests),
+    testCase(ViewTests.allTests)
 ])
